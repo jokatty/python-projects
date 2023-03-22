8a73c0e4e2c6906f1b92679a8ed98106f4c26c75
jokatty <jyotikattani@gmail.com>
2023-03-22 10:24:40 +0100

    modify greet route

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-  return "<p>hello, world</p>"
+    return "<p>hello, world</p>"
 
-@app.route("/greet")
-def greet():
-  return "welcome!"
+
+@app.route("/greet/user/<username>/<int:number>")
+def greet(username, number):
+    return f"welcome! {username}. you are user {number}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
